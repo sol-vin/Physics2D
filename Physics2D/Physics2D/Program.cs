6ec9133723b6588b15a338063397247a8e711445
redcodefinal <redcodefinal@gmail.com>
2013-05-07 09:45:10 -0700

    Added Monodevelop files

--- Physics2D/Physics2D/Program.cs
+++ Physics2D/Physics2D/Program.cs
@@ -2,20 +2,19 @@ using System;
 
 namespace Physics2D
 {
-#if WINDOWS || XBOX
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        static void Main(string[] args)
-        {
-            using (Game1 game = new Game1())
-            {
-                game.Run();
-            }
-        }
-    }
-#endif
+	static class Program
+	{
+		private static Game1 game;
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main ()
+		{
+			game = new Game1 ();
+			game.Run ();
+		}
+	}
 }
 
